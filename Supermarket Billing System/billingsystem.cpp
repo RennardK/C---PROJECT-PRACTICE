f063fa47e58805565b51dff60295bdef8c0974c0
RennardK <107853301+RennardK@users.noreply.github.com>
2023-02-02 11:39:01 +0300

    Update billingsystem.cpp

--- Supermarket Billing System/billingsystem.cpp
+++ Supermarket Billing System/billingsystem.cpp
@@ -15,12 +15,14 @@ class shopping{
         void buyer();
         void add();
         void edit();
-        void remove();
+        void rem();
         void list();
         void receipt();
 };
 
 void shopping :: menu(){
+    m:
+
     int choice;
     string email;
     string password;
@@ -65,7 +67,7 @@ void shopping :: menu(){
         // }
         else
         {
-            cout<<"Invalid login details!"
+            cout<<"Invalid email/password!"
         }
         break;
     case 2:
@@ -73,8 +75,103 @@ void shopping :: menu(){
         break;
 
     case 3:
-    
+        exit(0);
     default:
+        cout<<"Please select form the given options!";
         break;
+    }
+goto m;
+}
+
+void shopping:: administrator(){
+    m:
+    int choice;
+    cout<<"\n\n\n\t\t Administrator Menu";
+    cout<<"\n\t\t\t______1) Add product_____";
+    cout<<"\n\t\t\t                         ";
+    cout<<"\n\t\t\t______2) Modify product__";
+    cout<<"\n\t\t\t                         "; 
+    cout<<"\n\t\t\t______3) Delete product__";
+    cout<<"\n\t\t\t                         ";
+    cout<<"\n\t\t\t______4) Back to Main Menu";
+
+    cout<<"\n\n\tPlease enter your choice";
+    switch (choice)
+    {
+    case 1:
+        add();
+        break;
+    case 2:
+        edit();
+        break;
+    case 3:
+        rem();
+        break;
+    case 4:
+        menu();
+        break;
+    default:
+        cout<<"Invalid choice"<<endl;
+        break;
+    }
+goto m;
+}
+
+void shopping::buyer{
+    m:
+    int choice;
+    cout<<"\t\t\t     Buyer    \n";
+    cout<<"\t\t\t______________\n";
+    cout<<"                    \n";
+    cout<<"\t\t\t 1) Buy Product\n";
+    cout<<"                    \n";
+    cout<<"\t\t\t 2) Go Back   \n";
+    cout<<"                    \n";
+    cout<<"Enter your choice   \n";
+    cin>>choice;
+
+    switch (choice)
+    {
+    case 1:
+        receipt();
+        break;
+    case 2:
+        menu();
+        break;
+    default:
+        cout<<"Invalid choice";
+        break;
+    }
+goto m;
+}
+
+void shopping :: add(){
+    fstream data;
+    int c;
+    int token=0;
+    float p;
+    float d;
+    string n;
+
+    cout<<"\n\n\t\t\t  Add new product";
+    cout<<"\n\t\t\t Product code of the Product: ";
+    cin>>pcode;
+    cout<<"\n\t\t\tName of the Product";
+    cin>>pname;
+    cout<<"\n\t\t\tPrice of the Product";
+    cin>>price;
+    cout<<"\n\t\t\tDiscount on the Product";
+    cin>>dis;
+
+    data.open("database.txt", ios::in)
+    if (!data){ ///if the file doesn't exist, we then create the file
+        data.open("database.txt", ios::app|ios::out){
+            data<<" "<<pcode<<" "<<pname<<" "<<price<<" "<<dis<<endl;
+            data.close()
+        }
+    else{ // but if the file exist, we read from the fle with the variable declared above
+        data<<c<<n<<p<<d<<endl; // Initializing the index of file
+
+    }
     }
 }
